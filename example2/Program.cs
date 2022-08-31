e28691937214b5783e9a29a5cdd8e4006d8a2a08
29Tatiana <amiravob@mail.ru>
2022-08-31 13:06:21 +0300

    example2

---
+++ example2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа");
+string number1 = Console.ReadLine();
+string number2 = Console.ReadLine();
+int a = int.Parse(number1);
+int b = int.Parse(number2);
+if (a > b)
+    Console.WriteLine("min=" + b + ", max=" + a);
+else
+    Console.WriteLine("min=" + a + ", max=" + b);
