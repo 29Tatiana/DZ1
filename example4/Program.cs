3a06f589b7d16823c9502c3767fc3d5cb52aefdf
29Tatiana <amiravob@mail.ru>
2022-08-31 13:12:29 +0300

    example4

---
+++ example4/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите три числа");
+        string number1 = Console.ReadLine();
+        string number2 = Console.ReadLine();
+        string number3 = Console.ReadLine();
+        int a = int.Parse(number1);
+        int b = int.Parse(number2);
+        int c = int.Parse(number3);
+        int max = a;
+        if (b > max)
+            max = b;
+        if (c > max)
+            max = c;
+        Console.WriteLine(max);
+    }
+}
